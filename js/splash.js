d324540ad3b20c8f2d09091b965c37af13fc7258
wilcoxec <erika.wilcox@uky.edu>
2016-01-31 15:24:49 -0500

    Added Landing Page
    Added a splash page to enter site

---
+++ js/splash.js
@@ -0,0 +1,38 @@
+$(function(){
+
+  function start(){
+    $('#svgBox').animate({
+      height: '0px',
+      width: '0px'
+    }, 9000, 'linear');
+  }
+
+start();
+
+});
+
+
+/*$(document).ready(function() {
+
+  $(function restart(){
+    $('#svgBox').css({
+      height: '500px',
+      width: '200px',
+      opacity: 1
+    });
+
+    start
+  });
+
+  $(function start(){
+    $('#svgBox').delay(0).animate({
+    height: '0px',
+    width: '0px',
+    opacity: 0.25
+    }, 5000, 'linear');
+  });
+
+
+
+
+});*/
